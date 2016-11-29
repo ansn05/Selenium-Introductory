87d53b1a8f75d7298b50712b07cda3c62a22a2f3
ansn05 <ansn05@gmail.com>
2016-11-29 10:25:26 +0100

    Added project

---
+++ SeleniumTraining/SeleniumTraining/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTraining
+{
+    public class BaseTest
+    {
+        public string elementId;
+
+        public static void GoTo()
+        {
+            Driver.Instance.Navigate().GoToUrl("https://ansntest.wordpress.com/wp-admin/");
+        }
+
+        public static LoginCommand LoginAs(string userName)
+        {
+            return new LoginCommand(userName);
+        }
+
+        public BaseTest IsAtPage(string elementId)
+        {
+            this.elementId = elementId;
+            return this;
+        }
+    }
+}
---
+++ SeleniumTraining/SeleniumTraining/LoginTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumTraining
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Driver.Initialize();
+        }
+
+        [TestMethod]
+        public void CheckLoginPage()
+        {
+            BaseTest.GoTo();
+            Assert.IsTrue(
+                BaseTest.IsAtPage("");
+                //BaseTest. IsAtPage("Page down");
+        }
+
+        [TestMethod]
+        public  void LogInAsAdmin()
+        {
+            BaseTest.GoTo();
+            BaseTest.LoginAs("ansntest")
+                .withPassword("tester666")
+                .Login();
+        }
+    }
+}
